6e2770166cc37565b1cc81c402f0915c4a87d6a0
Zijian Yan <zi@zmba.local>
2023-05-16 15:38:53 -0400

    introduce openai api

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const path = require('path')
@@ -7,6 +9,17 @@ const cors = require('cors');
 
 app.use(cors({ origin: true }));
 
+
+const PROMPT = `
+Describe to me an activity or mental exercise for a child that will boost their self-esteem.
+Say it as if you were speaking to the child directly.
+Have a sense of humor.
+Make the activity something that requires the child's imaginatio.
+`
+
+console.log('process.env.OPENAI_API_KEY:', process.env.OPENAI_API_KEY)
+
+
 const port = process.env.PORT || 3000
 
 app.listen(port, ()=> {
@@ -14,6 +27,29 @@ app.listen(port, ()=> {
 })
 
 app.use(express.static(path.join(__dirname, '..', 'build')))
+
+app.get('/test', async (req, res, next)=> {
+
+    try {
+        const { Configuration, OpenAIApi } = require("openai");
+        const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+        });
+        const openai = new OpenAIApi(configuration);
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: PROMPT,
+            // temperature: 0,
+            max_tokens: 1000,   
+          });
+
+        const { text } = response?.data?.choices[0]
+        res.send(text)
+    } catch(err) {
+        res.send(err)
+    }
+
+})
 
 app.get('/', (req, res, next)=> {
     res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
