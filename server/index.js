319e32abc33d08911b345895ce1022f1cf9891bc
Zijian Yan <zi@zmba.local>
2023-05-16 15:14:49 -0400

    serve app on firebase

--- server/index.js
+++ server/index.js
@@ -1,19 +1,22 @@
 
 const express = require('express')
 const app = express()
+const path = require('path')
+const functions = require('firebase-functions')
+const cors = require('cors');
+
+app.use(cors({ origin: true }));
 
 const port = process.env.PORT || 3000
-
-const path = require('path')
 
 app.listen(port, ()=> {
     console.log(`listening on port ${port}`)
 })
 
-
 app.use(express.static(path.join(__dirname, '..', 'build')))
-
 
 app.get('/', (req, res, next)=> {
     res.sendFile(path.join(__dirname, '..', 'build', 'index.html'))
 })
+
+exports.app = functions.https.onRequest(app)
