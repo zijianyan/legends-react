9cc8942ccfaefd57f162c3b1fc6db92906b32516
Zijian Yan <zi@zmba.local>
2023-05-18 17:39:19 -0400

    install framer-motion

--- src/components/StartMood.js
+++ src/components/StartMood.js
@@ -1,18 +1,21 @@
 import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 
 export default function StartMood({ handleClickStartMood, moods }) {
     return (
-        <div id="start-mood">
-            <h1>Hey, how's it going today?</h1>
-            <ul className="moods">
-                {
-                    moods.map(({ score, src }) => {
-                        return  <li >
-                                    <img src={src} data-mood={score} onClick={handleClickStartMood} className="icon"/>
-                                </li>
-                    })
-                }
-            </ul>
-        </div>
+        <AnimatePresence>
+            <motion.div animate id="start-mood">
+                <h1>Hey, how's it going today?</h1>
+                <ul className="moods">
+                    {
+                        moods.map(({ score, src }) => {
+                            return  <li >
+                                        <img src={src} data-mood={score} onClick={handleClickStartMood} className="icon"/>
+                                    </li>
+                        })
+                    }
+                </ul>
+            </motion.div>
+        </AnimatePresence>
     )
 }
