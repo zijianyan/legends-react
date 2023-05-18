ef1a6b5b21ee5024f69eb3fd023e48f21745487b
Zijian Yan <zi@zmba.local>
2023-05-18 09:37:27 -0400

    update thank you text

--- src/components/Thanks.js
+++ src/components/Thanks.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Thanks () {
     return (
         <>
-            <h1>Thanks!</h1>
+            <h1>Thanks.</h1>
             <p>It was great playing together. See you tomorrow!</p>
         </>
     )
