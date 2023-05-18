bd00e2f982c598219323fc02a97a8fa527e8428b
Zijian Yan <zi@zmba.local>
2023-05-18 10:03:38 -0400

    separate concerns between youtube config and api call

--- server/api/youtube.js
+++ server/api/youtube.js
@@ -1,5 +1,8 @@
-const YOUTUBE_API_BASE_URL = 'https://www.googleapis.com/youtube/v3/search'
+function generateYouTubeUrl (topic) {
+    const YOUTUBE_API_BASE_URL = 'https://www.googleapis.com/youtube/v3/search'
+    return `${YOUTUBE_API_BASE_URL}?part=snippet&q=${encodeURIComponent(topic)+'kids'}&key=${process.env.YOUTUBE_API_KEY}`
+}
 
 module.exports = {
-    YOUTUBE_API_BASE_URL
+    generateYouTubeUrl
 }
--- server/index.js
+++ server/index.js
@@ -6,9 +6,9 @@ const app = express()
 const path = require('path')
 const axios = require('axios')
 
-const { GET_TOPICS_CONFIG, GET_ACTIVITY_CONFIG } = require('./api/openai/config')
-const { YOUTUBE_API_BASE_URL } = require('./api/youtube')
 const openai = require('./api/openai')
+const { GET_TOPICS_CONFIG, GET_ACTIVITY_CONFIG } = require('./api/openai/config')
+const { generateYouTubeUrl } = require('./api/youtube')
 
 const port = process.env.PORT || 3000
 app.listen(port, ()=> console.log(`listening on port ${port}`))
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, '..', 'build')))
 
 app.get('/topics', async (req, res, next)=> {
     try {
-        const response = await openai.createCompletion(GET_TOPICS_CONFIG()) // query openai for topics
+        const response = await openai.createCompletion(GET_TOPICS_CONFIG()) // query openai to generate topics
         const { choices } = response?.data
         res.send(choices)
     } catch (err) {
@@ -29,9 +29,9 @@ app.get('/topics', async (req, res, next)=> {
 app.get('/activity', async (req, res, next)=> { 
     const { topic } = req.query
     try {
-        const response = await openai.createCompletion(GET_ACTIVITY_CONFIG(topic)) // query openai for activity using topic
-        const youtube = await axios.get(`${YOUTUBE_API_BASE_URL}?part=snippet&q=${encodeURIComponent(topic)+'kids'}&key=${process.env.YOUTUBE_API_KEY}`) // search youtube for video using topic
-        const videoId = youtube.data.items[0].id.videoId
+        const response = await openai.createCompletion(GET_ACTIVITY_CONFIG(topic)) // query openai for activity based on topic
+        const video = await axios.get(generateYouTubeUrl(topic)) // query youtube for relevant video
+        const videoId = video.data.items[0].id.videoId
         const activityText = response?.data?.choices[0].text
         res.send({
             activityText,
