9ef229a62a0a1c5aecbbebc95b9b17f92c80c19b
Zijian Yan <zi@zmba.local>
2023-05-18 21:10:59 -0400

    adjust openai prompt

--- server/api/openai/config.js
+++ server/api/openai/config.js
@@ -3,12 +3,13 @@ const MODEL = "text-davinci-003"
 const GET_TOPICS_CONFIG = ()=> {
     const prompt = `
     Give me a fun topic for kids;
-    The topic should be very specific activity, but described in 5 words max;
-    Make it a bit quirky;
+    The topic should be described in 5 words max;
+    Make it a quirky and very specific activity;
     Max 1 topic;
     The should not be a topic that has been provided in the past 10 requests;
     Remove all quotation marks or punctuation;
     Remove all periods or "." from the text; 
+    Remove all dashes from the text;
     `
     return {
         model: MODEL,
@@ -21,18 +22,18 @@ const GET_TOPICS_CONFIG = ()=> {
 
 const GET_ACTIVITY_CONFIG = (topic) => { 
     const prompt = `
-    Describe to me an activity for a kid that will boost their self-esteem;
+    Describe to me a positive activity for a kid;
     Say it as if you were speaking to the child directly;
     Have a sense of humor;
     Make the activity something that invites the child's to use their imagination or creativity;
     Start off this text with "Great choice!";
     Be optimistic and encouraging;
-    This activity should be about ${topic}
+    This activity should be focused on ${topic}
     `
     return {
         model: MODEL,
         prompt,
-        temperature: 0.5,
+        temperature: 1,
         max_tokens: 1000,   
     }
   }
